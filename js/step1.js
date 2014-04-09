2af4bc72dfab120e96df26640e15e6f93e764eb5
Vincent <vinz2007@hotmail.com>
2014-04-10 01:33:11 +0200

    Colombia-EPR-agua-ODM

---
+++ js/step1.js
@@ -0,0 +1,181 @@
+function step1(){
+	g.selectAll("text")
+	.remove();
+
+
+	if(document.getElementById("selectedStep").innerHTML=="step3"){
+			svg.selectAll("rect")
+			.remove();
+
+		svg.selectAll("circle")
+			.remove();
+	};
+
+	if(document.getElementById("selectedStep").innerHTML=="step2"){
+		d3.json("data/water.json", function(error, data) {
+	        var colombiaData1=data.filter(function(d) {
+	            return(d.area=="Total" && d.year=="1993" && (d.country=="Colombia"))
+	        });
+	        
+	        var targetData=data.filter(function(d) {
+	            return(d.area=="Total" && d.year=="1993" && (d.country=="Meta ODM 2015" || d.country=="Meta Nacional 2015"))
+	        });
+			
+			
+			var bar = svg.selectAll(".bar")
+		      .data(colombiaData1)
+		      .transition().duration(2000)
+		      .attr("class", "bar")
+		      .attr("y", function(d) { return y(d.value); })
+		      .attr("height", function(d) { return height - y(d.value); });   
+
+		    document.getElementById("textToDisplay").innerHTML = "Desde 1993, Colombia ha desarrollado sustancialmente su infraestructura para proveer el acceso al suministro de agua potable y a los sistemas de saneamiento";    
+
+		    svg.selectAll("rect")
+		        .on("mouseover",function(d){
+
+		            d3.select(this).style("fill","lightgrey");
+
+		            var xPosition = parseFloat(d3.select(this).attr("x")) + document.getElementById("area1").offsetLeft + 1.5*margin.left ;
+		            var yPosition = parseFloat(d3.select(this).attr("y")) +document.getElementById("area1").offsetTop ;
+		             
+
+		            d3.select("#tooltip")
+		              .style("left", xPosition + "px")
+		              .style("top", yPosition + "px") 
+		              .select("#variableTootip")
+		              .text(function(){
+		                  return d.country;
+		              });
+
+		            d3.select("#tooltip")
+		              .select("#valueTooltip")
+		              .text(function(){
+		                  return d.value;
+		              });
+		            d3.select("#tooltip").classed("hidden", false);
+		        })
+		        .on("mouseout",function(d){
+
+		            d3.select(this).style("fill",color(d.area));
+
+		            d3.select("#tooltip").classed("hidden", true);
+		        });
+
+	 		document.getElementById("selectedStep").innerHTML="step1";
+		});
+
+	}else{
+
+		d3.json("data/water.json", function(error, data) {
+	        var colombiaData1=data.filter(function(d) {
+	            return(d.area=="Total" && d.year=="1993" && (d.country=="Colombia"))
+	        });
+			
+			
+			var bar = svg.selectAll(".bar")
+		      .data(colombiaData1)
+		      .enter().append("rect")
+		      .attr("class", "bar")
+		      .attr("x", function(d) { return x(d.indicator); })
+		      .attr("width", x.rangeBand())
+		      .attr("y", function(d) { return y(d.value); })
+		      .attr("height", function(d) { return height - y(d.value); })
+		      .style("fill",function(d) { return color(d.area);})
+		      .style("opacity",.9);   
+
+		    document.getElementById("textToDisplay").innerHTML = "Desde 1993, Colombia ha desarrollado sustancialmente su infraestructura para proveer el acceso al suministro de agua potable y a los sistemas de saneamiento";    
+
+		    svg.selectAll("rect")
+		        .on("mouseover",function(d){
+
+		            d3.select(this).style("fill","lightgrey");
+
+		            var xPosition = parseFloat(d3.select(this).attr("x")) + document.getElementById("area1").offsetLeft + 1.5*margin.left ;
+		            var yPosition = parseFloat(d3.select(this).attr("y")) +document.getElementById("area1").offsetTop ;
+		             
+
+		            d3.select("#tooltip")
+		              .style("left", xPosition + "px")
+		              .style("top", yPosition + "px") 
+		              .select("#variableTootip")
+		              .text(function(){
+		                  return d.country;
+		              });
+
+		            d3.select("#tooltip")
+		              .select("#valueTooltip")
+		              .text(function(){
+		                  return d.value;
+		              });
+		            d3.select("#tooltip").classed("hidden", false);
+		        })
+		        .on("mouseout",function(d){
+
+		            d3.select(this).style("fill",color(d.area));
+
+		            d3.select("#tooltip").classed("hidden", true);
+		        });
+
+	        
+	        var targetData=data.filter(function(d) {
+	            return(d.area=="Total" && d.year=="1993" && (d.country=="Meta ODM 2015" || d.country=="Meta Nacional 2015"))
+	        });
+
+
+		    var circle = svg.selectAll("circle")
+	            .data(targetData)
+	            .enter().append("circle")
+				.transition().delay(500).duration(500)
+	                .attr('cx', function(d) { return x(d.indicator)+x.rangeBand()/2;})
+	                .attr('cy', function(d) { return y(d.value); })
+	                .attr('r',5)
+	            .style("fill",function(d) { 
+	              return colorLimit(d.country)  
+	            })
+	            .style("opacity",1);
+
+		    svg.selectAll("circle")
+		        .on("mouseover",function(d){
+
+		            d3.select(this).style("fill","lightgrey");
+
+		            var xPosition = parseFloat(d3.select(this).attr("cx")) + document.getElementById("area1").offsetLeft + 1.1*margin.left ;
+		            var yPosition = parseFloat(d3.select(this).attr("cy")) +document.getElementById("area1").offsetTop ;
+		             
+
+		            d3.select("#tooltip")
+		              .style("left", xPosition + "px")
+		              .style("top", yPosition + "px") 
+		              .select("#variableTootip")
+		              .text(function(){
+		                  return d.country;
+		              });
+
+		            d3.select("#tooltip")
+		              .select("#valueTooltip")
+		              .text(function(){
+		                  return d.value;
+		              });
+		            d3.select("#tooltip").classed("hidden", false);
+		        })
+		        .on("mouseout",function(d){
+
+		            d3.select(this).style("fill",colorLimit(d.country));
+
+		            d3.select("#tooltip").classed("hidden", true);
+		        }); 
+
+	 		document.getElementById("selectedStep").innerHTML="step1";
+
+		});
+	};
+
+	g.append("text")
+		.attr("x",width-50)
+		.attr("y",30)
+		.text("1993")
+		.style("fill","grey")
+		.style("font","20px sans-serif")
+		.style("font-weight","bold");
+};
---
+++ js/step2.js
@@ -0,0 +1,207 @@
+function step2(){
+		g.selectAll("text")
+		.remove();
+
+if(document.getElementById("selectedStep").innerHTML=="step3"){
+	svg.selectAll("rect")
+		.remove();
+
+	svg.selectAll("circle")
+		.remove();
+	
+	d3.json("data/water.json", function(error, data) {
+        var colombiaData2=data.filter(function(d) {
+            return(d.area=="Total" && d.year=="2010" && (d.country=="Colombia"))
+        });
+        
+        var targetData=data.filter(function(d) {
+            return(d.area=="Total" && d.year=="1993" && (d.country=="Meta ODM 2015" || d.country=="Meta Nacional 2015"))
+        });
+
+
+		var bar = svg.selectAll(".bar")
+	      .data(colombiaData2)
+	      .enter().append("rect")
+	      .attr("class", "bar")
+	      .attr("x", function(d) { return x(d.indicator); })
+	      .attr("width", x.rangeBand())
+	      .attr("y", function(d) { return y(d.value); })
+	      .attr("height", function(d) { return height - y(d.value); })
+	      .style("fill",function(d) { return color(d.area);})
+	      .style("opacity",.9);  
+
+	    document.getElementById("textToDisplay").innerHTML = "En el 2010, Colombia estuvo cerca de cumplir con el Objetivo de Desarrollo del Milenio en acceso al agua potable, sin embargo se requiere una mayor inversión para lograr la menta en el acceso al saneamiento";         
+	
+	    svg.selectAll("rect")
+	        .on("mouseover",function(d){
+
+	            d3.select(this).style("fill","lightgrey");
+
+	            var xPosition = parseFloat(d3.select(this).attr("x")) + document.getElementById("area1").offsetLeft + 1.5*margin.left ;
+	            var yPosition = parseFloat(d3.select(this).attr("y")) +document.getElementById("area1").offsetTop ;
+	             
+
+	            d3.select("#tooltip")
+	              .style("left", xPosition + "px")
+	              .style("top", yPosition + "px") 
+	              .select("#variableTootip")
+	              .text(function(){
+	                  return d.country;
+	              });
+
+	            d3.select("#tooltip")
+	              .select("#valueTooltip")
+	              .text(function(){
+	                  return d.value;
+	              });
+	            d3.select("#tooltip").classed("hidden", false);
+	        })
+	        .on("mouseout",function(d){
+
+	            d3.select(this).style("fill",color(d.area));
+
+	            d3.select("#tooltip").classed("hidden", true);
+	        });
+
+
+
+	    var circle = svg.selectAll("circle")
+            .data(targetData)
+            .enter().append("circle")
+			.transition().delay(500).duration(500)
+                .attr('cx', function(d) { return x(d.indicator)+x.rangeBand()/2;})
+                .attr('cy', function(d) { return y(d.value); })
+                .attr('r',5)
+            .style("fill",function(d) { 
+              return colorLimit(d.country)  
+            })
+            .style("opacity",1);
+
+
+	    svg.selectAll("circle")
+	        .on("mouseover",function(d){
+
+	            d3.select(this).style("fill","lightgrey");
+
+	            var xPosition = parseFloat(d3.select(this).attr("cx")) + document.getElementById("area1").offsetLeft + 1.1*margin.left ;
+	            var yPosition = parseFloat(d3.select(this).attr("cy")) +document.getElementById("area1").offsetTop ;
+	             
+
+	            d3.select("#tooltip")
+	              .style("left", xPosition + "px")
+	              .style("top", yPosition + "px") 
+	              .select("#variableTootip")
+	              .text(function(){
+	                  return d.country;
+	              });
+
+	            d3.select("#tooltip")
+	              .select("#valueTooltip")
+	              .text(function(){
+	                  return d.value;
+	              });
+	            d3.select("#tooltip").classed("hidden", false);
+	        })
+	        .on("mouseout",function(d){
+
+	            d3.select(this).style("fill",colorLimit(d.country));
+
+	            d3.select("#tooltip").classed("hidden", true);
+	        });    	
+
+	});
+
+ 		document.getElementById("selectedStep").innerHTML="step2";
+
+}else{
+
+	d3.json("data/water.json", function(error, data) {
+        var colombiaData2=data.filter(function(d) {
+            return(d.area=="Total" && d.year=="2010" && (d.country=="Colombia"))
+        });
+		
+
+		var bar = svg.selectAll(".bar")
+	      .data(colombiaData2)
+	      .transition().duration(2000)
+	      .attr("class", "bar")
+	      .attr("y", function(d) { return y(d.value); })
+	      .attr("height", function(d) { return height - y(d.value); }); 
+
+	    document.getElementById("textToDisplay").innerHTML = "En el 2010, Colombia estuvo cerca de cumplir con el Objetivo de Desarrollo del Milenio en acceso al agua potable, sin embargo se requiere una mayor inversión para lograr la menta en el acceso al saneamiento";         
+	
+	    svg.selectAll("rect")
+	        .on("mouseover",function(d){
+
+	            d3.select(this).style("fill","lightgrey");
+
+	            var xPosition = parseFloat(d3.select(this).attr("x")) + document.getElementById("area1").offsetLeft + 1.5*margin.left ;
+	            var yPosition = parseFloat(d3.select(this).attr("y")) +document.getElementById("area1").offsetTop ;
+	             
+
+	            d3.select("#tooltip")
+	              .style("left", xPosition + "px")
+	              .style("top", yPosition + "px") 
+	              .select("#variableTootip")
+	              .text(function(){
+	                  return d.country;
+	              });
+
+	            d3.select("#tooltip")
+	              .select("#valueTooltip")
+	              .text(function(){
+	                  return d.value;
+	              });
+	            d3.select("#tooltip").classed("hidden", false);
+	        })
+	        .on("mouseout",function(d){
+
+	            d3.select(this).style("fill",color(d.area));
+
+	            d3.select("#tooltip").classed("hidden", true);
+	        });
+
+	    svg.selectAll("circle")
+	        .on("mouseover",function(d){
+
+	            d3.select(this).style("fill","lightgrey");
+
+	            var xPosition = parseFloat(d3.select(this).attr("cx")) + document.getElementById("area1").offsetLeft + 1.1*margin.left ;
+	            var yPosition = parseFloat(d3.select(this).attr("cy")) +document.getElementById("area1").offsetTop ;
+	             
+
+	            d3.select("#tooltip")
+	              .style("left", xPosition + "px")
+	              .style("top", yPosition + "px") 
+	              .select("#variableTootip")
+	              .text(function(){
+	                  return d.country;
+	              });
+
+	            d3.select("#tooltip")
+	              .select("#valueTooltip")
+	              .text(function(){
+	                  return d.value;
+	              });
+	            d3.select("#tooltip").classed("hidden", false);
+	        })
+	        .on("mouseout",function(d){
+
+	            d3.select(this).style("fill",colorLimit(d.country));
+
+	            d3.select("#tooltip").classed("hidden", true);
+	        });    	
+
+ 		document.getElementById("selectedStep").innerHTML="step2";
+	});
+
+};
+	g.append("text")
+		.attr("x",width-50)
+		.attr("y",30)
+		.text("2010")
+		.style("fill","grey")
+		.style("font","20px sans-serif")
+		.style("font-weight","bold");
+	
+}
